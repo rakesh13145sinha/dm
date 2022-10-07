0376306c3bf358263930bfeb34f59d34d0ebdf4c
rakesh13145sinha <rakeshsinha8292@gmail.com>
2022-10-08 00:37:17 +0530

    update local 1.4

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shadi.local.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shadi.production.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
