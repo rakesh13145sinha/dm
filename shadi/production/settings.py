4f72edea52d32eaa13603f06b757f2b42446e229
rakesh13145sinha <rakeshsinha8292@gmail.com>
2022-10-07 18:59:05 +0530

    06-58 07-10-2022

--- shadi/production/settings.py
+++ shadi/production/settings.py
@@ -16,17 +16,22 @@ WSGI_APPLICATION = 'shadi.production.wsgi.application'
 
 # Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'matrimony',
-        'USER': 'sanjitsinha',
-        'PASSWORD': 'sanjit@13145',
-        'HOST': '127.0.0.1',
-        'PORT': "5432"
-        }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME':'matrimony',
+#         'USER': 'sanjitsinha',
+#         'PASSWORD': 'sanjit@13145',
+#         'HOST': '127.0.0.1',
+#         'PORT': "5432"
+#         }
    
            
+# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
