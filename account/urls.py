1cd33206f2c6daa88bf8e530da53b7b8e7e1362f
rakesh13145sinha <rakeshsinha8292@gmail.com>
2022-10-07 19:18:01 +0530

    7:30

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include 
 from .views import *
 urlpatterns = [
-    path('signup',Registration.as_view(),name="registration")
+    path('signup',Registration.as_view(),name="registration"),
+    path('email',Check_Email.as_view()),
+    path('phone',Check_Phone_Number.as_view())
 ]
