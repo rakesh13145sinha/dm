f9f4aeb3e94cc8a3d8e07a47ed3149dc6e7776ac
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-01-11 14:53:34 +0530

    add q

--- connect/views.py
+++ connect/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework.decorators import api_view
 from account.models import Person 
-from django.db import Q
+from django.db.models import Q
 from .models import *
 
 
