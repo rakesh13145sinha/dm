834489307ea91a1ac15c7c742ebf29db78a7833d
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-01-12 14:30:25 +0530

    update admin pannel

--- Plan/admin.py
+++ Plan/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class PlanFeatureAdmin(admin.TabularInline):
+    model = PlanFeature
+
+class MemberShipAdmin(admin.ModelAdmin):
+   inlines = [PlanFeatureAdmin,]
+
+admin.site.register(MemberShip,MemberShipAdmin)
