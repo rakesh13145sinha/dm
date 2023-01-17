6d1c9d20324110b6a01fb08d31c06d12effdc60f
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-01-17 14:35:11 +0530

    udoate

--- adminuser/views.py
+++ adminuser/views.py
@@ -9,8 +9,9 @@ from django.db.models import Count,Q
 @api_view(['GET'])
 def dashboard(request):
     dashboard=Person.objects.\
-    annotate(male=Count("gender",filter=Q(gender="Male")),
+    aggregate(male=Count("gender",filter=Q(gender="Male")),
              female=Count("gender",filter=Q(gender="Female")),
              subscribe=Count("active_plan",filter=Q(active_plan='Trial')))
-    return Response({"data":dashboard.values("male","female","subscribe")})
+    return Response({"data":dashboard})
+    #return Response({"data":dashboard.values("male","female","subscribe")})
     
