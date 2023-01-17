53f9674299a677a7f42e0768eb134094814d8b0a
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-01-17 14:26:46 +0530

    update agrreegate

--- adminuser/views.py
+++ adminuser/views.py
@@ -3,13 +3,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from account.models import *
-from django.db.models import Count
+from django.db.models import Count,Q
+
 # Create your views here.
 @api_view(['GET'])
 def dashboard(request):
     dashboard=Person.objects.\
-    annotate(male=Count("gender",filter("Male")),
-             female=Count("gender",filter("Female")),
-             subscribe=Count("active_plan",filter('Trial')))
+    annotate(male=Count("gender",filter=Q(gender="Male")),
+             female=Count("gender",filter=Q(gender="Female")),
+             subscribe=Count("active_plan",filter=Q(active_plan='Trial')))
     return Response(dashboard)
     
