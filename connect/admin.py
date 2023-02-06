af1ae250450997e51614350bba1b94142d3f16e9
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-02-06 17:22:22 +0530

    update admin documentupload

--- connect/admin.py
+++ connect/admin.py
@@ -3,4 +3,9 @@ from .models import *
 # Register your models here.
 class UpdateRequestsAdmin(admin.ModelAdmin):
     list_display=['id','request_status','update_field_name']
+    
+class DocumentUploadAdmin(admin.ModelAdmin):
+    list_display=['id','upload_status','name_of_document',"profile","status"]
+    list_editable=['status']
 admin.site.register(UpdateRequests,UpdateRequestsAdmin)
+admin.site.register(DocumentUpload,DocumentUploadAdmin)
