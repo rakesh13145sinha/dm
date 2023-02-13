3745fcc76c40463ca5d981440b07a3f583dda692
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-02-13 18:24:10 +0530

    udate

--- account/views.py
+++ account/views.py
@@ -76,7 +76,7 @@ def visted_profiles(matrimonyid,requestid):
     """requested matrimony id"""
     requested_profile=get_object_or_404(Person,matrimony_id=requestid)
     
-    view_profile=selfprofile.viewedByMe.filter(view=requested_profile)
+    view_profile=selfprofile.me.filter(view=requested_profile)
     if view_profile:
         return True 
     else:
