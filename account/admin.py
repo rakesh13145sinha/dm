23c7504db6d59b2ffc9f2e4b2a3c453e50dcbb22
rakesh13145sinha <rakeshsinha8292@gmail.com>
2023-02-14 11:34:25 +0530

    udapte

--- account/admin.py
+++ account/admin.py
@@ -7,8 +7,9 @@ admin.site.register(SaveOTP)
 admin.site.register(BannerImage)
 admin.site.register(HomeScreenImage)
 class PersonAdmin(admin.ModelAdmin):
-    list_display=['id','matrimony_id','name','phone_number']
+    list_display=['id','matrimony_id','name','phone_number',"reg_date"]
     list_editable=['matrimony_id']
+    list_filter=['reg_date']
 class FriendRequestsAdmin(admin.ModelAdmin):
     list_display=['id','profile','requested_matrimony_id','request_status','status']
     list_editable=['status']
